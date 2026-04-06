53a0f7f9fb7f198c72df97115b04d91bbc1d1d0c
thienan230427 <thienan12342007@gmail.com>
2026-04-06 22:34:14 +0700

    Grace: Cap nhat 4 Poster phim moi tu local va Seeder theo yeu cau cua Sep An

--- database/seeders/MoviePosterUpdateSeeder.php
+++ database/seeders/MoviePosterUpdateSeeder.php
@@ -9,15 +9,15 @@ class MoviePosterUpdateSeeder extends Seeder
 {
     public function run(): void
     {
-        // Sử dụng các URL từ TheMovieDB (TMDB) vì chúng cực kỳ ổn định và cho phép hotlinking tốt hơn
-        $stablePosters = [
-            'Avengers: Secret Wars' => 'https://image.tmdb.org/t/p/w500/uS9mY7ew9v7YUyLqaX6AavpY66S.jpg',
-            'The Dark Knight' => 'https://image.tmdb.org/t/p/w500/qJ2tW6WMUDp9QmSbmz94S0OkiBh.jpg',
-            'Spider-Man: No Way Home' => 'https://image.tmdb.org/t/p/w500/1g0zzY8pS6Sia9vO3ZkyG3v3Wub.jpg',
-            'Dune: Part Two' => 'https://image.tmdb.org/t/p/w500/6iz9e9pU0mEovt0O03B9Wj62Sve.jpg',
+        // Sử dụng các ảnh Sếp vừa cung cấp từ folder public/images
+        $localPosters = [
+            'Avengers: Secret Wars' => '/images/pic1.jpg',
+            'The Dark Knight' => '/images/batmanpic.jpg',
+            'Spider-Man: No Way Home' => '/images/pic3nowayhome.jpg',
+            'Dune: Part Two' => '/images/pic2dune.jpg',
         ];
 
-        foreach ($stablePosters as $name => $url) {
+        foreach ($localPosters as $name => $url) {
             DB::table('movies')->where('name', $name)->update(['poster' => $url]);
         }
     }
